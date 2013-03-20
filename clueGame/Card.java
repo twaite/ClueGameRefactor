8e66ce7b61480069f57101ba4f6e84ea7de8a1bd
Timothy Waite <twaite@mines.edu>
2013-03-20 11:06:05 -0600

    Added new classes for player and card

---
+++ clueGame/Card.java
@@ -0,0 +1,37 @@
+package clueGame;
+
+public class Card{
+	public enum cardType { PERSON, WEAPON, ROOM };
+	private cardType type;
+	private String name;
+	
+	public Card() {
+		type = cardType.PERSON;
+		name = null;
+	}
+	
+	public Card(cardType type, String name) {
+		this.type = type;
+		this.name = name;
+	}
+	
+	public boolean equals(Object obj) {
+		return true;
+	}
+	
+	public cardType getType() {
+		return type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setType(cardType newType) {
+		type = newType;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
